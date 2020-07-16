b4bbf30fc46b87a01fb604c6ef6c6a92ef669f69
Arnav Singh <arnavbhaiya@gmail.com>
2020-07-16 22:29:51 +0200

    Created List View

---
+++ lib/screens/note_details.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class NoteDetails extends StatefulWidget {
+  const NoteDetails({Key key}) : super(key: key);
+
+  @override
+  _NoteDetailsState createState() => _NoteDetailsState();
+}
+
+class _NoteDetailsState extends State<NoteDetails> {
+  static var _priorities = ["High", "Low"];
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  //final TextStyle textStyle = Theme.of(context).textTheme.bodyText1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Edit Note"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: DropdownButton(
+                items: _priorities.map((String dropDown) {
+                  return DropdownMenuItem<String>(
+                    value: dropDown,
+                    child: Text(dropDown),
+                  );
+                }).toList(),
+                value: "Low",
+                onChanged: (valueSelected) {
+                  debugPrint("UserSelected $valueSelected");
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: TextField(
+                controller: titleController,
+                //style: ,
+                onChanged: (value) {
+                  debugPrint("Something chnaged");
+                },
+                decoration: InputDecoration(
+                    labelText: "Title",
+                    //labelStyle: //
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: TextField(
+                controller: descriptionController,
+                //  style: ,
+                onChanged: (value) {
+                  debugPrint("Something chnaged");
+                },
+                decoration: InputDecoration(
+                    labelText: "Description",
+                    //labelStyle: //
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+              ),
+            ),
+            //padding
+            Padding(
+              padding: const EdgeInsets.only(top: 15, bottom: 15),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: RaisedButton(
+                    color: Theme.of(context).accentColor,
+                    onPressed: () {
+                      setState(() {
+                        debugPrint("Save button clicked");
+                      });
+                    },
+                    elevation: 2,
+                    child: Text(
+                      "To Do",
+                      textScaleFactor: 1.5,
+                    ),
+                  )),
+                  Spacer(),
+                  Expanded(
+                    child: RaisedButton(
+                      color: Theme.of(context).accentColor,
+                      onPressed: () {
+                        setState(() {
+                          debugPrint("Delete button clicked");
+                        });
+                      },
+                      elevation: 2,
+                      child: Text(
+                        "Delete",
+                        textScaleFactor: 1.5,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/notelist.dart
@@ -0,0 +1,68 @@
+import 'package:NoteKeeper/screens/note_details.dart';
+import 'package:flutter/material.dart';
+
+class NoteList extends StatefulWidget {
+  const NoteList({Key key}) : super(key: key);
+
+  @override
+  _NoteListState createState() => _NoteListState();
+}
+
+int count = 0;
+
+class _NoteListState extends State<NoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes"),
+      ),
+      body: getListNoteView(),
+      //   floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        tooltip: 'Add Note',
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return NoteDetails();
+          }));
+        },
+      ),
+    );
+  }
+}
+
+ListView getListNoteView() {
+  // TextStyle titlestyle = Theme.of(ctx).textTheme.subtitle1;
+  return ListView.builder(
+    itemCount: count,
+    itemBuilder: (BuildContext context, int position) {
+      return Card(
+        color: Colors.white,
+        elevation: 2,
+        child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.yellow,
+              child: Icon(Icons.keyboard_arrow_right),
+            ),
+            title: Text(
+              "Dummy title",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            subtitle: Text("Dummy DATE"),
+            trailing: Icon(
+              Icons.delete,
+              color: Colors.grey,
+            ),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return NoteDetails();
+              }));
+            }),
+      );
+    },
+  );
+}
